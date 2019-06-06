efe5904918cbd2e4548cc1111bf8a9534ffa45e6
Nitzan Hen <nitzanhen7@gmail.com>
2019-06-06 12:14:07 +0300

    added the createPolygons() function

---
+++ create-polygons.js
@@ -0,0 +1,81 @@
+const util = require('util');
+const GeoJSON = require('geojson');
+const fs = require('fs');
+const JSITM = require('js-itm');
+
+const numVertices = 10;
+const default_radius = 100; //meters
+const default_field_of_view = 100; //Degrees
+
+module.exports = function () {
+	const polygons = [];
+
+	const cameras = require('./input.json');
+
+	/* 
+	 * Converting measurements:
+	 * Angles -> Radians
+	 * Meters -> LatLong degrees
+	 * X,Y -> LatLong
+	 */
+	var i = 0;
+	cameras.forEach(camera => {
+		camera.angle = toRadians(camera.angle);
+		camera.radius = toLatLongDegrees(default_radius);
+		if (!camera.field_of_view) camera.field_of_view = toRadians(default_field_of_view);
+
+		const gpsRef = JSITM.itmRef2gpsRef(Math.round(camera.x) + " " + Math.round(camera.y));
+		const latLong = gpsRef.split(" ");
+		camera.x = latLong[0]; //Latitude
+		camera.y = latLong[1]; //Longitude 
+
+		camera.id = i;
+		i += 1;
+	})
+
+	//Creating polygons
+	cameras.forEach(camera => {
+		const polygon = getPolygon(camera, numVertices - 1);
+		polygons.push(polygon);
+	});
+
+	const result = GeoJSON.parse(polygons, {
+		Polygon: 'coordinates'
+	});
+	return result;
+}
+// fs.writeFile('./output.geojson', util.inspect(result, {
+//	depth: null,
+//	maxArrayLength: null
+// }), () => console.log('success'))
+
+
+
+function getPolygon(camera, numVertices) {
+	var vertices = [];
+	const firstAngle = Math.PI / 2 + camera.angle - (camera.field_of_view / 2);
+	for (var i = 0; i < numVertices; i++) {
+		const angle = firstAngle + (camera.field_of_view / (numVertices - 1)) * i;
+		const newX = camera.x + camera.radius * Math.cos(angle);
+		const newY = camera.y + camera.radius * Math.sin(angle);
+		vertices.push([newX, newY]);
+	}
+
+	vertices.unshift([camera.x, camera.y]);
+	vertices.push([camera.x, camera.y])
+	const polygon = {
+		"id": camera.id,
+		"coordinates": vertices
+	};
+	return polygon;
+}
+
+//------ Utility Methods ------//
+
+function toLatLongDegrees(distance) {
+	return distance / 111111;
+}
+
+function toRadians(degrees) {
+	return degrees * (Math.PI / 180);
+}
--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const fs = require("fs");
 const path = require("path");
-const app = express();
 const itm = require("./js-itm");
+const createPolygons = require('./create-polygons');
+const util = require('util');
+
+const app = express();
 
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -18,12 +21,15 @@ app.get("/", (req, res) => {
   res.render("index.ejs", { cameras });
 });
 
-let rawdata = fs.readFileSync("public/cameras.json");
-let cameras = JSON.parse(rawdata);
-for (var i = 0; i < cameras.length; i++) {
-  var xy = Math.round(cameras[i].X) + " " + Math.round(cameras[i].Y);
-  newxy = JSITM.itmRef2gpsRef(xy).split(" ");
-  cameras[i].X = newxy[0];
-  cameras[i].Y = newxy[1];
-}
-//abcdabcd dan!
+
+const polygonJSON = createPolygons(); 
+console.log(util.inspect(polygonJSON, { depth: null, maxArrayLength: null}));
+
+// let rawdata = fs.readFileSync("public/cameras.json");
+// let cameras = JSON.parse(rawdata);
+// for (var i = 0; i < cameras.length; i++) {
+//   var xy = Math.round(cameras[i].X) + " " + Math.round(cameras[i].Y);
+//   newxy = JSITM.itmRef2gpsRef(xy).split(" ");
+//   cameras[i].X = newxy[0];
+//   cameras[i].Y = newxy[1];
+// }
